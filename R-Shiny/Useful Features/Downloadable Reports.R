9d8760b47908392946d0f0ce481cd96346877eb1
Holiday Tang <38384192+holiday007@users.noreply.github.com>
2019-12-30 15:24:34 -0600

    commit

---
+++ R-Shiny/Useful Features/Downloadable Reports.R
@@ -0,0 +1,61 @@
+library(shiny)
+
+######## ui ########
+ui = fluidPage(
+  title = 'Download a PDF report',
+  sidebarLayout(
+    sidebarPanel(
+      helpText(),
+      selectInput('x', 'Build a regression model of mpg against:',
+                  choices = names(mtcars)[-1]),
+      radioButtons('format', 'Document format', c('PDF', 'HTML', 'Word'),
+                   inline = TRUE),
+      downloadButton('downloadReport')
+    ),
+    mainPanel(
+      plotOutput('regPlot')
+    )
+  )
+)
+
+
+######## server #######
+server = function(input, output) {
+  
+  regFormula <- reactive({
+    as.formula(paste('mpg ~', input$x))
+  })
+  
+  output$regPlot <- renderPlot({
+    par(mar = c(4, 4, .1, .1))
+    plot(regFormula(), data = mtcars, pch = 19)
+  })
+  
+  output$downloadReport <- downloadHandler(
+    filename = function() {
+      paste('my-report', sep = '.', switch(
+        input$format, PDF = 'pdf', HTML = 'html', Word = 'docx'
+      ))
+    },
+    
+    content = function(file) {
+      src <- normalizePath('report.Rmd')
+      
+      # temporarily switch to the temp dir, in case you do not have write
+      # permission to the current working directory
+      owd <- setwd(tempdir())
+      on.exit(setwd(owd))
+      file.copy(src, 'report.Rmd', overwrite = TRUE)
+      
+      library(rmarkdown)
+      out <- render('report.Rmd', switch(
+        input$format,
+        PDF = pdf_document(), HTML = html_document(), Word = word_document()
+      ))
+      file.rename(out, file)
+    }
+  )
+  
+}
+
+shinyApp(ui, server)
